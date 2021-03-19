3d66c9888e9ad9ccfc6d9e476faacd262caae1c3
Judith Pillado <judith.pillado@gmail.com>
2021-03-19 16:04:56 -0500

    Add a conditional to make test pass

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,12 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      // undefined because in this particular test, there was only a name and not a color; therefore, the color = undefined! 
+      this.color = 'white';
+    } else {
+      this.color = color;
+    }
   }
 }
 
