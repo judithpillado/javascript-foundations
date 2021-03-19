e903acf0c32c24fdfbfff8b6a38c2039f36739b3
Judith Pillado <judith.pillado@gmail.com>
2021-03-19 18:04:10 -0500

    Create eatTally and eat methods to pass last test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -2,11 +2,19 @@ class Dragon {
   constructor(name, rider) {
     this.name = name;
     this.rider = rider;
-    this.hungry = true
+    this.hungry = true;
+    this.eatTally = 0
   }
 
   greet() {
     return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.eatTally += 1;
+    if (this.eatTally > 2) {
+      this.hungry = false;
+    }
   }
 }
 
